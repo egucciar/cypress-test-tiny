20d72ec6a6d819080d42202b8aab9f79af4009df
Erica Gucciardo <erica.gucciardo@observeinc.com>
2023-01-10 16:44:09 -0500

    repro

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,65 @@
 /// <reference types="cypress" />
-describe('page', () => {
-  it('works', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe("page", () => {
+  it("works", () => {
+    cy.visit("https://example.cypress.io");
+    cy.visit("https://example.cypress.io");
+    cy.visit("https://example.cypress.io");
+    cy.visit("https://example.cypress.io");
+    cy.visit("https://example.cypress.io");
+    cy.visit("https://example.cypress.io");
+    cy.visit("https://example.cypress.io");
+    cy.visit("https://example.cypress.io");
+    cy.visit("https://example.cypress.io");
+    cy.visit("https://example.cypress.io");
+    cy.visit("https://example.cypress.io");
+    cy.visit("https://example.cypress.io");
+    cy.visit("https://example.cypress.io");
+    cy.visit("https://example.cypress.io");
+    cy.visit("https://example.cypress.io");
+    cy.visit("https://example.cypress.io");
+    cy.visit("https://example.cypress.io");
+    cy.visit("https://example.cypress.io");
+    cy.visit("https://example.cypress.io");
+    cy.visit("https://example.cypress.io");
+    cy.visit("https://example.cypress.io");
+    cy.visit("https://example.cypress.io");
+    cy.visit("https://example.cypress.io");
+    cy.visit("https://example.cypress.io");
+    cy.visit("https://example.cypress.io");
+    cy.visit("https://example.cypress.io");
+    cy.visit("https://example.cypress.io");
+    cy.visit("https://example.cypress.io");
+    cy.visit("https://example.cypress.io");
+    cy.visit("https://example.cypress.io");
+    cy.visit("https://example.cypress.io");
+    cy.visit("https://example.cypress.io");
+    cy.visit("https://example.cypress.io");
+    cy.visit("https://example.cypress.io");
+    cy.visit("https://example.cypress.io");
+    cy.visit("https://example.cypress.io");
+    cy.visit("https://example.cypress.io");
+    cy.visit("https://example.cypress.io");
+    cy.visit("https://example.cypress.io");
+    cy.visit("https://example.cypress.io");
+    cy.visit("https://example.cypress.io");
+    cy.visit("https://example.cypress.io");
+    cy.visit("https://example.cypress.io");
+    cy.visit("https://example.cypress.io");
+    cy.visit("https://example.cypress.io");
+    cy.visit("https://example.cypress.io");
+    cy.visit("https://example.cypress.io");
+    cy.visit("https://example.cypress.io");
+    cy.visit("https://example.cypress.io");
+    cy.visit("https://example.cypress.io");
+    cy.visit("https://example.cypress.io");
+    cy.visit("https://example.cypress.io");
+    cy.visit("https://example.cypress.io");
+    cy.visit("https://example.cypress.io");
+    cy.visit("https://example.cypress.io");
+    cy.visit("https://example.cypress.io");
+    cy.visit("https://example.cypress.io");
+    cy.visit("https://example.cypress.io");
+    cy.visit("https://example.cypress.io");
+    cy.visit("https://example.cypress.io");
+  });
+});
